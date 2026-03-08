80b1d38ed081ea1dd78004d1fc3c7883e4794301
noahdavis05 <sc23nd@leeds.ac.uk>
2026-03-08 01:52:44 +0000

    Fixed bug fetching live telemetry data for the web UI

--- scheduler/pkg/telemetry/prometheus.go
+++ scheduler/pkg/telemetry/prometheus.go
@@ -323,7 +323,7 @@ func streamTelemetryToDashboard(promAPI promv1.API, nodes []*v1.Node) {
 		Query      string
 		MetricType string
 	}{
-		{`100 - (avg by(instance) (rate(node_cpu_seconds_total{mode="idle"}[10s])) * 100)`, "CPU"},
+		{`100 - (avg by(instance) (irate(node_cpu_seconds_total{mode="idle"}[1m])) * 100)`, "CPU"},
 		{`100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes))`, "RAM"},
 	}
 
